89e71237773071d6c8fa0319c9549dfcc694ae63
Carpe-Wang <1592622761@qq.com>
2022-03-22 18:52:36 +0800

    first commit

---
+++ src/github.com/CarpeWang/prepare/etcd_usage/demo1/demo1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"time"
+)
+
+func main() {
+	var (
+		config clientv3.Config
+		client *clientv3.Client
+		err    error
+	)
+
+	// 客户端配置
+	config = clientv3.Config{
+		Endpoints:   []string{"101.43.141.65:2379"},
+		DialTimeout: 5 * time.Second,
+	}
+
+	// 建立连接
+	if client, err = clientv3.New(config); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	client = client
+}
